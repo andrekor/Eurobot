25a79cf6a3a9e01fd3ba0d6c1e920da8be4201f4
André Kramer Orten <andrekor@ifi.uio.no>
2015-03-21 15:55:31 +0100

    Some tests with Ultrasound

--- maxSonar/maxSonar.ino
+++ maxSonar/maxSonar.ino
@@ -12,6 +12,7 @@ author: Bruce Allen
 
 #define ANALOGPIN 1
 #define PW_PIN 7
+#define PW_SCALE 50 //calculated pulse width scale
 
 double anVolt, inches, cm, pulse;
 int sum = 0;
@@ -33,38 +34,41 @@ void analog(){
 		//Used to read in the analog voltage output that is being sent by the maxSonar device
 		//Scale facros is (Vcc/512) per inch. 5V supply yields ~9.8mV/in
 		//Arduino analog pin goes from 0 to 1024, so the value has to be divided by 2 to het the actual inhes
-		anVolt = analogRead(ANALOGPIN)/2;
+		anVolt = analogRead(ANALOGPIN);
 		Serial.println(anVolt);
 		sum += anVolt;
 		delay(10);
 	}
 	inches = sum/avgRange;
 	cm = inches*2.54;
-	Serial.print("cm: ");
-	Serial.println(cm);
-	/*Serial.print(inches);
-	Serial.print("in, ");
+	//Serial.print("cm: ");
+	//Serial.println(cm);
+	/*Serial.print(inches)
+;	Serial.print("in, ");
 	Serial.print(cm);
 	Serial.println("cm");*/
 	sum = 0; 
-	delay(400);
+	//delay(400);
 }
 
-
+/*
+From calculation we have found a scalingfactor on pw/50 per cm. This is for white paper material. 
+*/
 void pw() {
 	pinMode(PW_PIN, INPUT);
 	//USed to read in the pulse that is being sent by the maxSonar device. 
 	//Pulse width representation with a scale factor of 147 us per Inch
 
 	pulse = pulseIn(PW_PIN, HIGH);
+	Serial.println(pulse);
 	//147us per inch
 	inches = pulse/147;
 	cm = inchToCm(inches);
-	Serial.print(inches);
+	/*Serial.print(inches);
 	Serial.print("in, ");
 	Serial.print(cm);
 	Serial.println("cm");
-
+*/
 }
 
 double inchToCm(double inch) {
